fa1508230166bba8b979f5feb20e55dd23a5b2d4
Jeremy Smith <j.smith.03@cantab.net>
2024-10-23 14:58:01 -0500

    Black formatting

--- full_search_spark.py
+++ full_search_spark.py
@@ -25,7 +25,7 @@ from timeit import default_timer as timer
 from utils import get_re
 from utils import output
 
-__author__ = 'Jeremy Smith'
+__author__ = "Jeremy Smith"
 
 
 def process(spark, data_dir, match_re, window=1, top=15):
@@ -39,34 +39,28 @@ def process(spark, data_dir, match_re, window=1, top=15):
         top - number of characters to return in count list
     """
     # Get json files and remove deletes
-    files = spark.read.json(os.path.join(data_dir, '*'), schema.value)
+    files = spark.read.json(os.path.join(data_dir, "*"), schema.value)
     files_filtered = files.filter(files.delete.isNull())
     cnt = files_filtered.count()
 
     # Match only to those with required character
-    tweets = files_filtered.filter(files_filtered.text.like(match_sql)) \
-                           .cache()
+    tweets = files_filtered.filter(files_filtered.text.like(match_sql)).cache()
 
     # Select all emoji from all tweet text
-    allemoji = files_filtered.rdd \
-                             .flatMap(lambda row: EMOJI_UNICODE_SET
-                                      .intersection(list(row.text))) \
-                             .map(lambda t: (t, 1))
+    allemoji = files_filtered.rdd.flatMap(
+        lambda row: EMOJI_UNICODE_SET.intersection(list(row.text))
+    ).map(lambda t: (t, 1))
 
     # Count tweets
     tweet_cnt = tweets.count()
 
     # Count languages
-    langs_cnt = tweets.groupBy('lang').count().collect()
+    langs_cnt = tweets.groupBy("lang").count().collect()
 
     # Perform the regex search
-    results = tweets.rdd \
-                    .flatMap(lambda row: re.findall(match_re, row.text)) \
-                    .cache()
-    before = results.filter(lambda t: (t[0] in EMOJI_UNICODE_SET)) \
-                    .map(lambda t: (t[0], 1))
-    after = results.filter(lambda t: (t[-1] in EMOJI_UNICODE_SET)) \
-                   .map(lambda t: (t[-1], 1))
+    results = tweets.rdd.flatMap(lambda row: re.findall(match_re, row.text)).cache()
+    before = results.filter(lambda t: (t[0] in EMOJI_UNICODE_SET)).map(lambda t: (t[0], 1))
+    after = results.filter(lambda t: (t[-1] in EMOJI_UNICODE_SET)).map(lambda t: (t[-1], 1))
 
     # Count matches
     match_cnt = results.count()
@@ -74,104 +68,116 @@ def process(spark, data_dir, match_re, window=1, top=15):
     after_cnt = after.count()
 
     # Take ordered list of before and after character counts
-    allemoji_top = allemoji.reduceByKey(add) \
-                           .takeOrdered(top, key=lambda x: -x[1])
-    before_top = before.reduceByKey(add) \
-                       .takeOrdered(top, key=lambda x: -x[1])
-    after_top = after.reduceByKey(add) \
-                     .takeOrdered(top, key=lambda x: -x[1])
+    allemoji_top = allemoji.reduceByKey(add).takeOrdered(top, key=lambda x: -x[1])
+    before_top = before.reduceByKey(add).takeOrdered(top, key=lambda x: -x[1])
+    after_top = after.reduceByKey(add).takeOrdered(top, key=lambda x: -x[1])
 
-    summary_dict = {'before_top': before_top,
-                    'after_top': after_top,
-                    'all_emoji_top': allemoji_top,
-                    'match_count': match_cnt,
-                    'tweet_count': tweet_cnt,
-                    'total_count': cnt,
-                    'before_count': before_cnt,
-                    'after_count': after_cnt,
-                    'language_counts': langs_cnt}
+    summary_dict = {
+        "before_top": before_top,
+        "after_top": after_top,
+        "all_emoji_top": allemoji_top,
+        "match_count": match_cnt,
+        "tweet_count": tweet_cnt,
+        "total_count": cnt,
+        "before_count": before_cnt,
+        "after_count": after_cnt,
+        "language_counts": langs_cnt,
+    }
 
     return summary_dict
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         description="Search a Twitter archive (from archive.org) to find the "
-                    "characters which occur before and after a chosen target. "
-                    "Uses Spark for distributed searching.",
-        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+        "characters which occur before and after a chosen target. "
+        "Uses Spark for distributed searching.",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
     parser.add_argument(
-        '-p', '--data_path',
-        default='small_data',
-        help="Path to the Twitter archive")
+        "-p", "--data_path", default="small_data", help="Path to the Twitter archive"
+    )
+    parser.add_argument("-m", "--emoji_match", default="pistol", help="Emoji character to match")
+    parser.add_argument("-w", "--window", type=int, default=1, help="Window size for adjacency")
     parser.add_argument(
-        '-m', '--emoji_match',
-        default='pistol',
-        help="Emoji character to match")
-    parser.add_argument(
-        '-w', '--window',
-        type=int,
-        default=1,
-        help="Window size for adjacency")
-    parser.add_argument(
-        '-t', '--top',
-        type=int,
-        default=15,
-        help="Number of top characters to output")
+        "-t", "--top", type=int, default=15, help="Number of top characters to output"
+    )
     args = parser.parse_args()
 
     # Match character, SQL search and regex search
-    match = EMOJI_UNICODE[':{}:'.format(args.emoji_match)]
-    match_sql = '%{}%'.format(match)
+    match = EMOJI_UNICODE[":{}:".format(args.emoji_match)]
+    match_sql = "%{}%".format(match)
     match_re = get_re(match, window=args.window)
 
     print("Running on data   : {}".format(args.data_path))
     print("Match             : {}  {}".format(args.emoji_match, match))
 
     # Build SparkSession
-    spark = SparkSession.builder \
-                        .master('local[*]') \
-                        .appName('full_search_spark') \
-                        .config('spark.debug.maxToStringFields', 100) \
-                        .getOrCreate()
+    spark = (
+        SparkSession.builder.master("local[*]")
+        .appName("full_search_spark")
+        .config("spark.debug.maxToStringFields", 100)
+        .getOrCreate()
+    )
     sc = spark.sparkContext
 
     # A reduced json tweet schema for fields of interest
-    schema = sc.broadcast(StructType([
-        StructField('id', LongType(), True),
-        StructField('text', StringType(), True),
-        StructField('lang', StringType(), True),
-        StructField('retweet_count', LongType(), True),
-        StructField('favorite_count', LongType(), True),
-        StructField('created_at', StringType(), True),
-        StructField('geo', StructType([
-            StructField('coordinates', ArrayType(DoubleType(), True), True),
-            StructField('type', StringType(), True)
-            ]), True),
-        StructField('delete', StructType([
-            StructField('status', StructType([
-                StructField('id', LongType(), True),
-                StructField('id_str', StringType(), True),
-                StructField('user_id', LongType(), True),
-                StructField('user_id_str', StringType(), True)
-                ]), True)
-            ]), True)
-        ]))
+    schema = sc.broadcast(
+        StructType(
+            [
+                StructField("id", LongType(), True),
+                StructField("text", StringType(), True),
+                StructField("lang", StringType(), True),
+                StructField("retweet_count", LongType(), True),
+                StructField("favorite_count", LongType(), True),
+                StructField("created_at", StringType(), True),
+                StructField(
+                    "geo",
+                    StructType(
+                        [
+                            StructField("coordinates", ArrayType(DoubleType(), True), True),
+                            StructField("type", StringType(), True),
+                        ]
+                    ),
+                    True,
+                ),
+                StructField(
+                    "delete",
+                    StructType(
+                        [
+                            StructField(
+                                "status",
+                                StructType(
+                                    [
+                                        StructField("id", LongType(), True),
+                                        StructField("id_str", StringType(), True),
+                                        StructField("user_id", LongType(), True),
+                                        StructField("user_id_str", StringType(), True),
+                                    ]
+                                ),
+                                True,
+                            )
+                        ]
+                    ),
+                    True,
+                ),
+            ]
+        )
+    )
 
     # Main process
     start_t = timer()
-    result = process(spark, args.data_path, match_re,
-                     window=args.window, top=args.top)
+    result = process(spark, args.data_path, match_re, window=args.window, top=args.top)
     end_t = timer()
 
     spark.stop()
 
     print("Elapsed Time          : {:.3f} min".format((end_t - start_t) / 60))
-    print("Total Tweets          : {:d}".format(result['total_count']))
-    print("Total Tweets w/ Match : {:d}".format(result['tweet_count']))
-    print("Total Match Chars     : {:d}".format(result['match_count']))
-    print("Total w/ Before       : {:d}".format(result['before_count']))
-    print("Total w/ After        : {:d}".format(result['after_count']))
+    print("Total Tweets          : {:d}".format(result["total_count"]))
+    print("Total Tweets w/ Match : {:d}".format(result["tweet_count"]))
+    print("Total Match Chars     : {:d}".format(result["match_count"]))
+    print("Total w/ Before       : {:d}".format(result["before_count"]))
+    print("Total w/ After        : {:d}".format(result["after_count"]))
 
     # Output results
     output(result, disp=True)
--- src/utils.py
+++ src/utils.py
@@ -7,7 +7,7 @@ Utility functions for Spark Twitter search.
 import re
 import pandas as pd
 
-__author__ = 'Jeremy Smith'
+__author__ = "Jeremy Smith"
 
 
 def get_re(match, window=1):
@@ -18,10 +18,9 @@ def get_re(match, window=1):
         match  - character to match
         window - find this many characters before and after the match
     """
-    word_or_emoji_re = "(['#@]?\w[\w'-]*|\W)?"
+    word_or_emoji_re = r"(['#@]?\w[\w'-]*|\W)?"
 
-    r = "{} " + (window - 1) * "*?{} " \
-        + "*?({}) " + (window - 1) * "*{} " + "*{}"
+    r = "{} " + (window - 1) * "*?{} " + "*?({}) " + (window - 1) * "*{} " + "*{}"
     a = [word_or_emoji_re] * window + [match] + [word_or_emoji_re] * window
 
     return re.compile(r.format(*a))
@@ -36,14 +35,10 @@ def output(result, disp=True):
     """
 
     # Convert output to dataframes
-    df_before = pd.DataFrame(
-        result['before_top'], columns=['Emoji', 'CountBefore'])
-    df_after = pd.DataFrame(
-        result['after_top'], columns=['Emoji', 'CountAfter'])
-    df_allemoji = pd.DataFrame(
-        result['all_emoji_top'], columns=['Emoji', 'Count'])
-    df_lang = pd.DataFrame(
-        result['language_counts'], columns=['Lang', 'Count'])
+    df_before = pd.DataFrame(result["before_top"], columns=["Emoji", "CountBefore"])
+    df_after = pd.DataFrame(result["after_top"], columns=["Emoji", "CountAfter"])
+    df_allemoji = pd.DataFrame(result["all_emoji_top"], columns=["Emoji", "Count"])
+    df_lang = pd.DataFrame(result["language_counts"], columns=["Lang", "Count"])
 
     if disp:
         print(df_before)
@@ -52,16 +47,10 @@ def output(result, disp=True):
         print(df_allemoji)
 
     # Merge before and after dataframes
-    df_all = pd.merge(df_before, df_after, on='Emoji', how='outer')
+    df_all = pd.merge(df_before, df_after, on="Emoji", how="outer")
 
     # Output csv files
-    df_all.to_csv("./outputs/data.csv",
-                  encoding='utf-8',
-                  index=False)
-    df_lang.to_csv("./outputs/langdata.csv",
-                   encoding='utf-8',
-                   index=False)
-    df_allemoji.to_csv("./outputs/alldata.csv",
-                       encoding='utf-8',
-                       index=False)
+    df_all.to_csv("./outputs/data.csv", encoding="utf-8", index=False)
+    df_lang.to_csv("./outputs/langdata.csv", encoding="utf-8", index=False)
+    df_allemoji.to_csv("./outputs/alldata.csv", encoding="utf-8", index=False)
     return
