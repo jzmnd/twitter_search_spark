e80106dce8718c85e54a8131791b7fd52686fa6e
Jeremy Smith <j.smith.03@cantab.net>
2024-10-23 15:29:36 -0500

    Fixing spark submit

--- setup-submit.sh
+++ setup-submit.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
 
-export SPARK_HOME="/usr/local/Cellar/apache-spark/2.2.1/libexec"
+export SPARK_HOME="/opt/homebrew/opt/apache-spark/libexec"
 
+pip install -U -t dependencies -r requirements.txt
+cp dependencies/emoji/unicode_codes/emoji.json ./emoji.json
+
+cd src
+zip -r ../src.zip .
+cd ..
+
+# Modify this to run with your own data or emoji match
 spark-submit --master local[*] \
-             --py-files utils.py \
+             --py-files src.zip \
+             --files emoji.json \
              full_search_spark.py \
              --data_path "small_data/01" \
-             --emoji_match pistol \
+             --emoji_match water_pistol \
              --window 1 \
              --top 15
--- src/unicode_codes.py
+++ src/unicode_codes.py
@@ -4,10 +4,16 @@
 """
 Data literal storing emoji names and unicode codes
 """
-from emoji.unicode_codes import EMOJI_DATA
-
+import json
 
 __all__ = ["EMOJI_UNICODE", "UNICODE_EMOJI", "EMOJI_UNICODE_SET"]
+
+
+with open("emoji.json", "r") as f:
+    # Load from the emoji.json file rather than directly importing from the emoji library since
+    # Spark has issues finding this file when inside a zipped python dependency.
+    # This also avoids having to ship the whole library to Spark and instead just the data we need.
+    EMOJI_DATA = json.load(f)
 
 UNICODE_EMOJI = {k: v["en"] for k, v in EMOJI_DATA.items()}
 EMOJI_UNICODE = {v: k for k, v in UNICODE_EMOJI.items()}
